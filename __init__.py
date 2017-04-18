61026206a7cdf5f3cad721dc009be92c3f809b7d
Grit pi <krit405f@gmail.com>
2017-04-18 15:54:42 +0700

    by tee

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 import checknet
 import status
+import socket
+import subprocess
+import time
+from threading import Thread
 from  firebase import db
 #from multiprocessing import Process
 #print(status.getStatus())
@@ -17,12 +21,33 @@ with open ("config","r") as file:
 	uid=file.read()
 if checknet.is_connected() and uid is not '' :
 	print("==========Online===========")
+        
+	db.child("users").child(uid).child("status").update(status.getStatus())
 	#firebase.sendToFirebase(status.getStatus())
-	try:
-		db.child("users").child(uid).child("status").update(status.getStatus())
-	except :
-		print("Not found uid")
+#	try:
+
+		#db.child("users").child(uid).child("status").update(status.getStatus())
+		#print("Debug")
+		#time.sleep(5)
+		#print("Debug2")
+		#status.runCommand()
+                #db.child("users").child(uid).child("status").update(status.getStatus())
+#	except :
+#		print("Not found uid")
 else:
 	print("==========Offline===========")
-
+	server = socket.socket()         
+	#HOSTNAME = subprocess.getoutput("hostname -I | cut -d' ' -f1")
+	HOSTNAME = socket.gethostbyname(socket.gethostname())
+	PORT = 55555               
+	server.bind((HOSTNAME,PORT))      
+	server.listen(5)
+	print("Wait connect.....")
+	client,addr = server.accept()    
+	print('Connection from', addr)   
+	#data=status.getStatus()
+	data="Helooooooooooooooooooooo"
+	while True:
+		client.send(data.encode('utf-8'))
+	client.close()   
 
